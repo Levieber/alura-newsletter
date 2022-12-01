bc34422ffb037b38b04afd2be73170e70a02b4ee
Levi Eber <gblevieber@gmail.com>
2022-11-30 22:59:10 -0300

    chore: adding Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1>Hello React and Tailwind</h1>
+      <h1 className="text-6xl font-bold underline text-blue-800">Hello React and Tailwind</h1>
     </>
   );
 };
